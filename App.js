1238a84c2bfbdfeb2318e0dbd0699b610ba347bb
Sik <sikeliodev@gmail.com>
2022-05-11 00:08:14 +0200

    add icons and settings page

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import type {Node} from 'react';
 import {
     Alert,
@@ -18,63 +10,107 @@ import {
     StatusBar,
     StyleSheet,
     Text,
+    TextInput,
     useColorScheme,
     View,
 } from 'react-native';
+
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 import SelectDropdown from 'react-native-select-dropdown'
+
 import Slider from '@react-native-community/slider';
+import Config from "react-native-config";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import io from 'socket.io-client';
 
 import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
+    Colors, Header
 } from 'react-native/Libraries/NewAppScreen';
 
 const {width} = Dimensions.get('window');
 
 const Separator = () => (
-    <View style={styles.separator} />
+    <View style={Styles.separator} />
 );
 
-const Section = ({children, title}): Node => {
-    const isDarkMode = useColorScheme() === 'dark';
+const Stack = createNativeStackNavigator();
+
+const App: () => Node = () => {
     return (
-        <View style={styles.sectionContainer}>
-        <Text
-            style={[
-            styles.sectionTitle,
-            {
-                color: isDarkMode ? Colors.white : Colors.black,
-            },
-            ]}>
-            {title}
-        </Text>
-        <Text
-            style={[
-            styles.sectionDescription,
-            {
-                color: isDarkMode ? Colors.light : Colors.dark,
-            },
-            ]}>
-            {children}
-        </Text>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="Sikontrol"
+                    component={Sikontrol}
+                    options={{
+                        title: "Sikontrol",
+                        headerStyle: {
+                            backgroundColor: '#2e6abb'
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold'
+                        }
+                    }}
+                />
+                <Stack.Screen
+                    name="Settings"
+                    component={Settings}
+                    options={({ route }) => ({
+                        title: "Paramètres",
+                        headerStyle: {
+                            backgroundColor: '#2e6abb'
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold'
+                        }
+                    })}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+const Settings = () => {
+    let sToken;
+    const [name, setName] = useState('')
+
+    function submit (sToken) {
+        if (sToken) {
+            Alert.alert(sToken);
+        }
+    }
+
+    return (
+        <View style={[Styles.container, {flexDirection: "column"}]}>
+            <View style={{alignItems: 'center'}}>
+                <TextInput
+                    secureTextEntry={true}
+                    placeholder="Token"
+                    onChangeText={(sValue) => sToken = sValue}
+                    style={Styles.dropdownBtnStyle}
+                />
+                <Separator />
+                <Button
+                    title='Mettre à jour'
+                    onPress={() => {submit(sToken)}}
+                />
+            </View>
         </View>
     );
 };
 
-const App: () => Node = () => {
+const Sikontrol = ({ navigation }) => {
     const isDarkMode = useColorScheme() === 'dark';
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-    const sTokenJSON = "1234";
+    const sTokenJSON = Config.SERVER_TOKEN;
 
-    const nIp = "192.168.0.16", nPort = 3000;
+    const nIp = Config.SERVER_IP, nPort = Config.SERVER_PORT;
     const sUrl = `ws://${nIp}:${nPort}`;
 
     const ioSocket = io.connect(sUrl, {
@@ -84,6 +120,19 @@ const App: () => Node = () => {
         withCredentials: true,
         transports: ['websocket'],
         reconnectionAttempts: 15
+    });
+
+    ioSocket.on('connect_error', () => {
+        Alert.alert(
+            "Serveur introuvable",
+            "Le serveur n'est peut être pas démarré ou soit vous avez mal configuré les paramètres",
+            [
+                {
+                    text: "OK",
+                    onPress: () => console.log("OK Pressed")
+                }
+            ]
+        );
     });
 
     let aApp = [], sActualApp;
@@ -97,7 +146,7 @@ const App: () => Node = () => {
     });
 
     return (
-        <View style={[styles.container, {flexDirection: "column"}]}>
+        <View style={[Styles.container, {flexDirection: "column"}]}>
             <View style={{flexDirection: 'column', flex: 0.075}}>
                 <Button
                     title="Précédent"
@@ -126,6 +175,8 @@ const App: () => Node = () => {
                 />
             </View>
 
+            <Separator />
+
             <View style={{flexDirection: 'column', flex: 0.075}}>
                 <Slider
                     minimumValue={0}
@@ -140,7 +191,6 @@ const App: () => Node = () => {
                     })}
                 />
             </View>
-
             <View style={{alignItems: 'center'}}>
                 <SelectDropdown
                     data={aApp}
@@ -154,17 +204,19 @@ const App: () => Node = () => {
                     rowTextForSelection={(sSelectedApp) => {
                         return sSelectedApp
                     }}
-                    buttonStyle={styles.dropdownBtnStyle}
-                    buttonTextStyle={styles.dropdownBtnTxtStyle}
+                    buttonStyle={Styles.dropdownBtnStyle}
+                    buttonTextStyle={Styles.dropdownBtnTxtStyle}
                     renderDropdownIcon={isOpened => {
                         return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#444'} size={18} />;
                     }}
                     dropdownIconPosition={'right'}
-                    dropdownStyle={styles.dropdownDropdownStyle}
-                    rowStyle={styles.dropdownRowStyle}
-                    rowTextStyle={styles.dropdownRowTxtStyle}
+                    dropdownStyle={Styles.dropdownDropdownStyle}
+                    rowStyle={Styles.dropdownRowStyle}
+                    rowTextStyle={Styles.dropdownRowTxtStyle}
                 />
             </View>
+
+            <Separator />
 
             <View style={{flexDirection: 'column', flex: 1}}>
                 <Slider
@@ -180,11 +232,20 @@ const App: () => Node = () => {
                     })}
                 />
             </View>
-        </View>
-      );
-};
 
-const styles = StyleSheet.create({
+            <View>
+                <Button
+                    title='Paramètres'
+                    onPress={() => {
+                        navigation.navigate('Settings');
+                    }}
+                />
+            </View>
+        </View>
+    );
+}
+
+const Styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
@@ -192,7 +253,7 @@ const styles = StyleSheet.create({
 
     separator: {
         marginVertical: 8,
-        borderBottomColor: '#737373',
+        borderBottomColor: 'transparent',
         borderBottomWidth: StyleSheet.hairlineWidth,
     },
 
